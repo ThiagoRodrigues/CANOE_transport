74cde8c7de9cb9d8635c202a65a4238df071d8bf
ThiagoRodrigues <thiago.rodrigues@utoronto.ca>
2026-04-27 11:54:01 -0400

    initial commit 2

--- transport_sector.py
+++ transport_sector.py
@@ -1,6 +1,38 @@
 import pandas as pd
 
 
+# Create the technologies for the transport sector from a CVS file 
+
+# Add Existing Capacities: input_files just add the raw data from NRCan or 
+# miniter of transport, and the we create a script (existing_capacity.py) to process 
+# it and make it into the table Existing Capacity
+
+# Add Demands: input_files just add the raw data from NRCan and GDP, 
+# and the we create a script (transport_demand.py) to process (@Dijuan)
+
+# C2A and Limit Annual Capacity Factor (@Dijuan) 
+
+# Survival Curve (@Dijuan)
+
+# Costs Variable/Fixed/Investment (@Rashid check with @LihWei to 
+# see what changed in the BEV Chrg) 
+
+# Emissions (@Rashid)
+
+# Efficiency (@Rashid)
+
+# InputSlip (@Rashid)
+
+# DataSource (@All)
+
+# Embodied Emissions (@Rashid)
+
+# LifeTime just for insfrastructure and some vechiles (like buses) e.g., chargers (@Rashid)
+
+# CapacityFactorTech (@Rashid)
+
+
+
 
 def build_transport_sector():
     pass
